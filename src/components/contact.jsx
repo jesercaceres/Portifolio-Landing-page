b84df13f788e4cb7247562c56d6e555739f61e64
Jéser Cáceres Marcelino <jeser.caceres01@yahoo.com>
2026-03-11 02:18:32 -0300

    feat(contact): substitui formulario de e-mail por botao de whatsapp

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,103 +1,31 @@
-import { useState } from "react";
-import emailjs from "emailjs-com";
 import React from "react";
 
-const initialState = {
-  name: "",
-  email: "",
-  message: "",
-};
 export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setState((prevState) => ({ ...prevState, [name]: value }));
-  };
-  const clearState = () => setState({ ...initialState });
-  
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(name, email, message);
-    
-    {/* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ }
-    
-    emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
-      .then(
-        (result) => {
-          console.log(result.text);
-          clearState();
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
   return (
     <div>
       <div id="contact">
         <div className="container">
-          <div className="col-md-8">
+          <div className="col-md-5 col-md-offset-1">
             <div className="row">
               <div className="section-title">
-                <h2>Fale conosco</h2>
+                <h2>Faça um orçamento já!</h2>
                 <p>
-                  Preencha o formulário abaixo para nos enviar um e-mail. Responderemos o mais rápido possível.
+                  Clique no botão abaixo para entrar em contato comigo pelo WhatsApp.
                 </p>
               </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        placeholder="Nome"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="E-mail"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    rows="4"
-                    placeholder="Mensagem"
-                    required
-                    onChange={handleChange}
-                  ></textarea>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div id="success"></div>
-                <button type="submit" className="btn btn-custom btn-lg">
-                  Enviar mensagem
-                </button>
-              </form>
+              <div style={{ marginBottom: "50px" }}>
+                <a
+                  href="https://wa.me/5515998617010"
+                  className="btn btn-custom btn-lg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-whatsapp"></i> Falar no WhatsApp
+                </a>
+              </div>
             </div>
           </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
+          <div className="col-md-4 col-md-offset-1 contact-info">
             <div className="contact-item">
               <h3>Informações de contato</h3>
               <p>
@@ -129,18 +57,13 @@ export const Contact = (props) => {
               <div className="social">
                 <ul>
                   <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
+                    <a href="https://www.linkedin.com/in/jesercaceres/" target="_blank" rel="noopener noreferrer">
+                      <i className="fa fa-linkedin"></i>
                     </a>
                   </li>
                   <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
+                    <a href="https://github.com/jesercaceres" target="_blank" rel="noopener noreferrer">
+                      <i className="fa fa-github"></i>
                     </a>
                   </li>
                 </ul>
