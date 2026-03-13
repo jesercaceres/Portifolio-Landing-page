142e0f83ddd8b587aff5852090e3755665acb263
Jéser Cáceres Marcelino <jeser.caceres01@yahoo.com>
2026-03-13 11:07:25 -0300

    FIX: Adjust experience responsivity for all breakpoints.

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -6,29 +6,33 @@ import "./experience.css";
 export const Experience = (props) => {
   return (
     <div id="experience" className="text-center">
-      <div className="container">
+      <div className="container experience-container">
         <div className="section-title">
           <h2>Trajetória</h2>
         </div>
         <div className="row">
-          {props.data ? (
-            <VerticalTimeline className="experience-timeline">
-              {props.data.map((d, i) => (
-                <VerticalTimelineElement
-                  key={`${d.title}-${i}`}
-                  date={d.date}
-                  iconStyle={{ background: "#ffffff", color: "#5ca9fb" }}
-                  contentArrowStyle={{ borderRight: "7px solid #ffffff" }}
-                >
-                  <h3 className="vertical-timeline-element-title">{d.title}</h3>
-                  <h4 className="vertical-timeline-element-subtitle">{d.company}</h4>
-                  <p>{d.description}</p>
-                </VerticalTimelineElement>
-              ))}
-            </VerticalTimeline>
-          ) : (
-            "loading"
-          )}
+          <div className="col-xs-12">
+            <div className="experience-timeline-container">
+              {props.data ? (
+                <VerticalTimeline className="experience-timeline">
+                  {props.data.map((d, i) => (
+                    <VerticalTimelineElement
+                      key={`${d.title}-${i}`}
+                      date={d.date}
+                      iconStyle={{ background: "#ffffff", color: "#5ca9fb" }}
+                      contentArrowStyle={{ borderRight: "7px solid #ffffff" }}
+                    >
+                      <h3 className="vertical-timeline-element-title">{d.title}</h3>
+                      <h4 className="vertical-timeline-element-subtitle">{d.company}</h4>
+                      <p>{d.description}</p>
+                    </VerticalTimelineElement>
+                  ))}
+                </VerticalTimeline>
+              ) : (
+                "loading"
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
